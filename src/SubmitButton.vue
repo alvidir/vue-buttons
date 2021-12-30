b4dd6795b884dc3d856ed68ee2041b8028b4094f
HectorMRC <thehector1593@gmail.com>
2021-12-30 23:24:17 +0100

    refactoring for submit button

--- src/SubmitButton.vue
+++ src/SubmitButton.vue
@@ -1,12 +1,14 @@
 <template>
   <button id="submit-button"
           class="round-corners fib-5"
-          :class="{disabled: disabled, off: loading}"
+          :class="{disabled: disabled,
+                   off: loading,
+                   large: large}"
           @click="onClick">
     <slot v-if="!loading"></slot>
     <div v-else>
       <slot :name="spinner">
-        <pulse-loader :size="'8px'" :radius="'5px'" :color="SPINNER_COLOR"></pulse-loader>
+        <pulse-loader :size="'8px'" :radius="'5px'" color="#ffffff80"></pulse-loader>
       </slot>
     </div>
   </button>
@@ -17,7 +19,6 @@ import { defineComponent } from "vue"
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
 const SUBMIT_EVENT_NAME = "submit"
-const SPINNER_COLOR = "#FFFFFF80"
 
 export default defineComponent({
   name: "SubmitButton",
@@ -27,20 +28,9 @@ export default defineComponent({
   },
   
   props: {
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  setup() {
-    return {
-      SPINNER_COLOR
-    }
+    large: Boolean,
+    loading: Boolean,
+    disabled: Boolean,
   },
 
   methods: {
@@ -64,16 +54,20 @@ $on-hover-submit-color: lighten($submit-color,  $fib-4 * 1%) !default;
 
 #submit-button {
     position: relative;
-  
     height: $fib-9 * 1px;
     width: 100%;
-    
     border: $fib-1 * 1px solid;
     border-color: $border-color;
-    font-size: medium; 
+    font-size: medium;
+    color: #ffffffd0;
 
     transition: font-size $fib-7 * 0.01s,
                 height $fib-7 * 0.01s;    
+
+    &.large {
+      height: $fib-9 * 1px !important;
+      font-size: 1rem !important;
+    }
 
     &:not(.disabled){
         &:not(.off) {
@@ -93,6 +87,7 @@ $on-hover-submit-color: lighten($submit-color,  $fib-4 * 1%) !default;
     &.disabled {
       height: $fib-8 * 1px;
       font-size: smaller;
+      color: darken($disabled-color, $fib-6 * 1%);
 
       transition: background $fib-8 * 0.01s,
                   border-color $fib-8 * 0.01s,
