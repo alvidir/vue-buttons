5e536da1833e3933558b1c0beb0de01378cc6695
Hector <thehector1593@gmail.com>
2022-12-31 20:11:05 +0100

    chore: do not use any on include method

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { App } from "vue";
 import RegularButton from "./RegularButton.vue";
 import SubmitButton from "./SubmitButton.vue";
 import SwitchButton from "./SwitchButton.vue";
@@ -6,15 +7,17 @@ const REGULAR_BUTTON_TAG = "regular-button";
 const SUBMIT_BUTTON_TAG = "submit-button";
 const SWITCH_BUTTON_TAG = "switch-button";
 
+function include(app: App): App {
+  return app
+    .component(REGULAR_BUTTON_TAG, RegularButton)
+    .component(SUBMIT_BUTTON_TAG, SubmitButton)
+    .component(SWITCH_BUTTON_TAG, SwitchButton);
+}
+
 export default {
   RegularButton,
   SubmitButton,
   SwitchButton,
 };
 
-export function include(app: any): any {
-  app
-    .component(REGULAR_BUTTON_TAG, RegularButton)
-    .component(SUBMIT_BUTTON_TAG, SubmitButton)
-    .component(SWITCH_BUTTON_TAG, SwitchButton);
-}
+export { include, REGULAR_BUTTON_TAG, SUBMIT_BUTTON_TAG, SWITCH_BUTTON_TAG };
